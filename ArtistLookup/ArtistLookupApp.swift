db0a2bda6c18b3b6a99f4e19810c903585575858
Brandon Knox <bknox.dev@gmail.com>
2021-04-14 18:00:22 -0700

    Initial Commit

---
+++ ArtistLookup/ArtistLookupApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtistLookupApp.swift
+//  ArtistLookup
+//
+//  Created by Brandon Knox on 4/14/21.
+//
+
+import SwiftUI
+
+@main
+struct ArtistLookupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
